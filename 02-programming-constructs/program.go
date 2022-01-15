1ec59fc49a5b17abbf54a3bc070bbcd2006dc6d9
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-15 13:00:23 +0530

    switch case construct

--- 02-programming-constructs/program.go
+++ 02-programming-constructs/program.go
@@ -43,4 +43,95 @@ func main() {
 		no += no
 	}
 	fmt.Printf("no = %d\n", no)
+
+	//switch construct
+	/*
+		rank by score
+		score 0 - 3 => "Terrible"
+		score 4 - 7 => "Not Bad"
+		score 8 - 9 => "Good"
+		score 10 => "Excellent"
+		otherwise => "Invalid Score"
+	*/
+
+	score := 6
+	/*
+		switch score {
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+			fmt.Println("Terrible")
+		case 4:
+		case 5:
+		case 6:
+			fmt.Println("Not Bad")
+		case 7:
+		case 8:
+		case 9:
+			fmt.Println("Good")
+		case 10:
+			fmt.Println("Excellent")
+		default:
+			fmt.Println("Invalid Score")
+		}
+	*/
+	switch score {
+	case 0, 1, 2, 3:
+		fmt.Println("Terrible")
+	case 4, 5, 6:
+		fmt.Println("Not Bad")
+	case 7, 8, 9:
+		fmt.Println("Good")
+	case 10:
+		fmt.Println("Excellent")
+	default:
+		fmt.Println("Invalid Score")
+	}
+
+	myVar := 22
+	/*
+		switch myVar % 2 {
+		case 0:
+			fmt.Println("even")
+		case 1:
+			fmt.Println("odd")
+		}
+	*/
+	switch {
+	case myVar%2 == 0:
+		fmt.Println("even")
+	case myVar%2 == 1:
+		fmt.Println("odd")
+	}
+
+	//fallthrough
+	n := 4
+	switch n {
+	case 0:
+		fmt.Println("is zero")
+	case 1:
+		fmt.Println("n <= 1")
+		fallthrough
+	case 2:
+		fmt.Println("n <= 2")
+		fallthrough
+	case 3:
+		fmt.Println("n <= 3")
+		fallthrough
+	case 4:
+		fmt.Println("n <= 4")
+		fallthrough
+	case 5:
+		fmt.Println("n <= 5")
+		fallthrough
+	case 6:
+		fmt.Println("n <= 6")
+	case 7:
+		fmt.Println("n <= 7")
+		fallthrough
+	case 8:
+		fmt.Println("n <= 8")
+	}
+
 }
