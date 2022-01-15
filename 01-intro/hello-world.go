e6f070161e738691704791f2dea759b43d2114f4
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-15 12:17:48 +0530

    hello world & data types

---
+++ 01-intro/hello-world.go
@@ -0,0 +1,16 @@
+/* package declaration */
+package main
+
+/* import dependent packages */
+import "fmt"
+
+/* package level variables */
+
+/* package init function */
+
+/* main function */
+func main() {
+	fmt.Println("Hello World!")
+}
+
+/* other functions */
