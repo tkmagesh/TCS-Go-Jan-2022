b8f8bb6c2d62d7542f18516be763113273b81f02
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-15 16:53:55 +0530

    Update 08-demo.go

--- 03-functions/08-demo.go
+++ 03-functions/08-demo.go
@@ -22,11 +22,6 @@ func subtract(x, y int) int {
 	return x - y
 }
 
-func process(x, y int, oper func(int, int) int) {
-	result := oper(x, y)
-	fmt.Println(result)
-}
-
 func getLoggedOper(oper func(int, int) int) func(int, int) {
 	return func(x, y int) {
 		fmt.Println("Before invocation")
