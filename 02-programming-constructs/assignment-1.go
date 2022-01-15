08ab60bd2d9a5d6dce474a5d0eb916f7dfb02061
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-15 14:25:56 +0530

    assignment-1 solution

---
+++ 02-programming-constructs/assignment-1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 3; i <= 100; i++ {
+		isPrime := true
+		for j := 2; j < i; j++ {
+			if i%j == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			fmt.Println(i)
+		}
+	}
+}
