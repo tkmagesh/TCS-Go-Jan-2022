363c1154180e33c27b4873685ee21a9784eaa1bb
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-16 10:05:08 +0530

    Update 10-demo.go

--- 03-functions/10-demo.go
+++ 03-functions/10-demo.go
@@ -12,24 +12,22 @@ func main() {
 }
 
 func f1() {
-	defer func() {
-		fmt.Println("deferred from f1 - [1]")
-	}()
-	defer func() {
-		fmt.Println("deferred from f1 - [2]")
-	}()
-	defer func() {
-		fmt.Println("deferred from f1 - [3]")
-	}()
+	defer fmt.Println("deferred from f1 - [1]")
+	defer fmt.Println("deferred from f1 - [2]")
+	defer fmt.Println("deferred from f1 - [3]")
 	fmt.Println("f1 started")
-	f2()
+	result := f2()
+	fmt.Println("result from f2 = ", result)
 	fmt.Println("f1 completed")
 }
 
-func f2() {
+func f2() (result int) {
 	defer func() {
 		fmt.Println("deferred from f2")
+		result = 500
 	}()
 	fmt.Println("f2 started")
 	fmt.Println("f2 completed")
+	result = 100
+	return
 }
