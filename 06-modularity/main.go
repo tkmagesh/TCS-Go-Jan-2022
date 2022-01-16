78dd7db93ec2a01d2c86fa3c75374f3609ac3366
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-16 15:00:32 +0530

    using 3rd party packages

--- 06-modularity/main.go
+++ 06-modularity/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"modularity-demo/calc"
 	"modularity-demo/calc/utils"
+
+	"github.com/fatih/color"
 )
 
 func main() {
 	fmt.Println(calc.Add(100, 200))
 	fmt.Println(calc.Subtract(100, 200))
-	fmt.Println("Operation Count : ", calc.GetOpCount())
+	color.Red(fmt.Sprintln("Operation Count : ", calc.GetOpCount()))
 
-	fmt.Println("Is 97 prime? :", utils.IsPrime(97))
+	color.Blue(fmt.Sprintln("Is 97 prime? :", utils.IsPrime(97)))
 }
