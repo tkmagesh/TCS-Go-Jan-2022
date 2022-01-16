63cfc312fe5483a6b94cba32bd57dbbd880466c5
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-16 12:34:44 +0530

    assignment:6 pointers

---
+++ 05-pointers/program.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var no int = 10
+	/* noPtr  => a variable used to store the "address" of an int value */
+	var noPtr *int = &no
+	fmt.Println(noPtr)
+
+	//deferencing => accessing the value using the address (pointer)
+	var x = *noPtr
+	fmt.Println(x)
+
+	//incrementing no
+	fmt.Println("Before incrementing, no = ", no)
+	increment(&no)
+	fmt.Println("After incrementing, no = ", no)
+
+	//assignment:6
+	var n1, n2 = 10, 20
+	fmt.Println("Before swapping n1 = ", n1, " n2 = ", n2)
+	swap( /*  */ )
+	fmt.Println("Before swapping n1 = ", n1, " n2 = ", n2)
+}
+
+func increment(n *int) {
+	(*n)++
+}
+
+func swap( /*  */ ) {
+
+}
