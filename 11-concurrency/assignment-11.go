d22d8aa5bbb752a30f66eb70dfb36bb302d566d9
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-22 16:16:33 +0530

    assignment:11 solution (incomplete)

--- 11-concurrency/assignment-11.go
+++ 11-concurrency/assignment-11.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -22,13 +23,23 @@ import (
 		The loop should remain in the print function
 */
 func main() {
-	print("Hello")
-	print("World")
+	x := make(chan bool)
+	y := make(chan bool)
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go print("Hello", x, y, wg)
+	go print("World", y, x, wg)
+	x <- true
+	wg.Wait()
 }
 
-func print(s string) {
+func print(s string, in, out chan bool, wg *sync.WaitGroup) {
 	for i := 0; i < 5; i++ {
+		<-in
 		time.Sleep(500 * time.Millisecond)
 		fmt.Println(s)
+		out <- true
 	}
+	fmt.Println(s, " - Done")
+	wg.Done()
 }
