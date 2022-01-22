6477c8262ce6d6132bfe112e754be1b097f6d2bb
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-22 15:23:39 +0530

    channels read & write

--- 11-concurrency/demo-09.go
+++ 11-concurrency/demo-09.go
@@ -3,26 +3,23 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
 func main() {
-	wg := &sync.WaitGroup{}
+
 	resultCh := make(chan int)
-	wg.Add(1)
-	go add(100, 200, resultCh, wg)
+	go add(100, 200, resultCh)
 	result := <-resultCh
-	wg.Wait()
 	fmt.Println("Result = ", result)
 
 }
 
-func add(x, y int, resultCh chan int, wg *sync.WaitGroup) {
+func add(x, y int, resultCh chan int) {
 	fmt.Printf("processing %d and %d\n", x, y)
 	time.Sleep(2 * time.Second)
 	result := x + y
 	fmt.Println("returing result : ", result)
 	resultCh <- result
-	wg.Done()
+
 }
