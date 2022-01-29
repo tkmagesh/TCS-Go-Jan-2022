fde446cfcc6e20cf74ddbc096fa96aa80f9847c9
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-29 12:54:27 +0530

    server streaming implementation

--- 15-grpc-app/client/client.go
+++ 15-grpc-app/client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"grpc-app/proto"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
@@ -15,15 +16,43 @@ func main() {
 		log.Fatal(err)
 	}
 	client := proto.NewAppServiceClient(clientConn)
+	ctx := context.Background()
 
+	//doRequestResponse(ctx, client)
+	doServerStreaming(ctx, client)
+}
+
+func doRequestResponse(ctx context.Context, client proto.AppServiceClient) {
 	req := &proto.AddRequest{
 		X: 100,
 		Y: 200,
 	}
-	ctx := context.Background()
+
 	res, err := client.Add(ctx, req)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	fmt.Printf("Adding 100 and 200, result = %d\n", res.GetResult())
+}
+
+func doServerStreaming(ctx context.Context, client proto.AppServiceClient) {
+	req := &proto.PrimeRequest{
+		Start: 3,
+		End:   100,
+	}
+	stream, err := client.GeneratePrimes(ctx, req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		primeNo := res.GetPrimeNumber()
+		fmt.Println("Prime no received : ", primeNo)
+	}
 }
--- 15-grpc-app/server/server.go
+++ 15-grpc-app/server/server.go
@@ -6,6 +6,7 @@ import (
 	"grpc-app/proto"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -23,6 +24,38 @@ func (s *server) Add(ctx context.Context, req *proto.AddRequest) (*proto.AddResp
 		Result: result,
 	}
 	return res, nil
+}
+
+func (s *server) GeneratePrimes(req *proto.PrimeRequest, stream proto.AppService_GeneratePrimesServer) error {
+	start := req.GetStart()
+	end := req.GetEnd()
+	fmt.Printf("Request receive for generating prime from %d to %d\n", start, end)
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			time.Sleep(500 * time.Millisecond)
+			res := &proto.PrimeResponse{
+				PrimeNumber: no,
+			}
+			fmt.Println("Sending prime no : ", no)
+			stream.Send(res)
+		}
+	}
+	return nil
+}
+
+func isPrime(no int32) bool {
+	if no <= 0 {
+		return false
+	}
+	if no <= 3 {
+		return true
+	}
+	for i := int32(2); i <= no-1; i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
